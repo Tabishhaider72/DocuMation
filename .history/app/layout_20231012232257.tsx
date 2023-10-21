99d04927a3b1b1e3d2ed0a030ef7c91deaa26ce8
SayedTabish72 <sayedtabish72@gmail.com>
2023-10-22 00:11:52 +0530

    new changs

--- .history/app/layout_20231012232257.tsx
+++ .history/app/layout_20231012232257.tsx
@@ -12,7 +12,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'DocuMation',
+  title: 'Jotion',
   description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
@@ -45,7 +45,7 @@ export default function RootLayout({
               defaultTheme="system"
               enableSystem
               disableTransitionOnChange
-              storageKey="DocuMation-theme-2"
+              storageKey="jotion-theme-2"
             >
               <Toaster position="bottom-center" />
               <ModalProvider />
